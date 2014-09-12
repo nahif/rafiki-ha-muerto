4eab53efd15142a64c6eee997ccd347a249064e3
vrdragicevic <vrdragicevic@gmail.com>
2014-09-12 10:59:02 -0300

    camara + controller

---
+++ Assets/Scripts/CamaraBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamaraBehaviour : MonoBehaviour {
+
+	public Transform Player;
+	
+	public Vector2 Margin, Smoothing;
+	
+	//public BoxCollider2D Bounds;
+	
+	//public Vector3 _min,_max;
+	
+	public bool IsFollowing { get; set; }
+	
+	// Use this for initialization
+	void Start () {
+
+}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = transform.position.x;
+		var y = transform.position.y;
+		/*if (IsFollowing) {
+			Debug.Log ("hola");
+			if(Mathf.Abs(x - Player.position.x) > Margin.x)
+			   x = Mathf.Lerp(x, Player.position.x,Smoothing.x * Time.deltaTime);
+			if(Mathf.Abs(y -Player.position.y) > Margin.y)
+			   y = Mathf.Lerp(y, Player.position.y,Smoothing.y * Time.deltaTime);
+		}
+
+		var cameraHalfWidth = camera.orthographicSize * ((float)Screen.width / Screen.height);
+
+		x = Mathf.Clamp (x, _min.x + cameraHalfWidth, _max.x - cameraHalfWidth);
+		y = Mathf.Clamp (y, _min.y + camera.orthographicSize, _max.y - camera.orthographicSize);
+		*/
+		
+		x = Mathf.Lerp(x, Player.position.x,Smoothing.x * Time.deltaTime);
+		y = Mathf.Lerp(y, Player.position.y,Smoothing.y * Time.deltaTime);
+		
+		//x = Player.position.x;
+		//y = Player.position.y;
+		
+		
+		
+		transform.position = new Vector3 (x, y, transform.position.z);
+		
+	}
+}
