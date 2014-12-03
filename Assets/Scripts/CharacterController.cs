c84640d12db9972e373fe6d41d116defbea7140e
Vicente Dragicevic <vrdragicevic@gmail.com>
2014-12-03 20:07:40 -0300

    Suavizado movimiento!

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 
 public class CharacterController : MonoBehaviour {
-
-	public float maxspeed=10f;
+	
+	public float maxspeed=5f;
 	public float deathChangeSpeed = 40f;
 	public float actualspeed=0;
 	private float lastYSpeed;
@@ -12,6 +12,7 @@ public class CharacterController : MonoBehaviour {
 	Animator anim;
 	Animator Mainanim;
 	bool grounded;
+	bool justGrounded;
 	float groundedRadius = .2f;
 	[SerializeField] LayerMask whatIsGround;
 	[SerializeField] LayerMask whatIsPlatform;
@@ -19,9 +20,9 @@ public class CharacterController : MonoBehaviour {
 	Transform headCheck;
 	//Cambios comportamiento segun piso
 	GroundCheckController gcc;
-
-
-
+	
+	
+	
 	void Start(){
 		Mainanim = GetComponent<Animator> ();
 		anim = GameObject.Find("cuerpo").GetComponent<Animator> ();
@@ -29,18 +30,27 @@ public class CharacterController : MonoBehaviour {
 		Physics2D.IgnoreLayerCollision (LayerMask.NameToLayer("Interact"),LayerMask.NameToLayer("Ignore Raycast"),true);
 		//Esto ignora la colision del personaje (Que esta en IGnore Raycast) y las semillas
 	}
-
+	
 	void  Awake() {
 		groundCheck = transform.Find ("GroundCheck");
 		headCheck = transform.Find ("HeadCheck");
 	}
 	
-	// Update is called once per frame
+	float t = 0f;
+	float lastAngle = 0f;
 	void FixedUpdate () {
 		if (Mathf.Abs (rigidbody2D.velocity.y - lastYSpeed) >= deathChangeSpeed)
 			Die ();
 		lastYSpeed = rigidbody2D.velocity.y;
-		grounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsGround);
+		if (Physics2D.OverlapCircle (groundCheck.position, groundedRadius, whatIsGround)) {
+			if (!grounded) {
+				grounded = true;
+			}
+			t=0f;
+		} else {
+			if (grounded) t=0f;
+			grounded = false;
+		}
 		//Movimiento horizontal
 		if (burried) return;
 		float move = Input.GetAxis ("Horizontal");
@@ -49,14 +59,14 @@ public class CharacterController : MonoBehaviour {
 			rigidbody2D.velocity = new Vector2 (maxspeed * move, rigidbody2D.velocity.y);
 		} else if (materialName == "Resbaladiso") {
 			gcc.delay = Mathf.Lerp (gcc.delay, maxspeed * move, gcc.friction * gcc.friction * Time.deltaTime);
-   			rigidbody2D.velocity = new Vector2 (gcc.delay, rigidbody2D.velocity.y);
- 		} else {
+			rigidbody2D.velocity = new Vector2 (gcc.delay, rigidbody2D.velocity.y);
+		} else {
 			rigidbody2D.velocity = new Vector2 (maxspeed * move, rigidbody2D.velocity.y);
 		}
-
+		
 		anim.SetFloat ("Speed",Mathf.Abs( maxspeed * move));
 		anim.speed = Mathf.Abs (maxspeed * move)/5;
-
+		
 		if (move > 0 && !facingRight) {
 			Flip();		
 		}
@@ -65,46 +75,56 @@ public class CharacterController : MonoBehaviour {
 		}
 		if(grounded) {
 			int layermask=~((1<<LayerMask.NameToLayer("Interact")) | Physics2D.IgnoreRaycastLayer); //VA A IGNORAR INTERACT O PODEMOS PONER QUE SOLO PESQUE GROUND Y COLISION
-			RaycastHit2D hit = Physics2D.Raycast (transform.position, -transform.up,Mathf.Infinity,layermask);
+			RaycastHit2D hit = Physics2D.Raycast (groundCheck.position, -transform.up,1f,layermask);
 			if (hit.collider != null && hit.collider.tag!="Player") {
 				//Debug.Log(Mathf.Atan2(hit.normal.y,hit.normal.x)*90/Mathf.PI);
 				float angle=transform.eulerAngles.z;
 				float angle2=Mathf.Atan2(hit.normal.y,hit.normal.x)*90/Mathf.PI-45;
+				if (lastAngle != angle2) { //Cambio de angulo
+					t += Time.deltaTime;
+					Quaternion quat = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(angle2,Vector3.forward), t/0.2f);
+					float newAngle = quat.eulerAngles.z;
+					transform.rotation = quat;
+					lastAngle = newAngle;
+				}
+				
 				//if(angle<0)angle+=2*3.14f;
 				//if(angle2<0)angle2+=2*3.14f;
-				transform.rotation=Quaternion.AngleAxis(angle2,Vector3.forward);
+				//transform.rotation=Quaternion.AngleAxis(angle2,Vector3.forward);
 			}
-
+			
 		} else {
+			
+			t += Time.deltaTime;
 			anim.SetFloat ("Speed",0);
-			transform.rotation=Quaternion.AngleAxis(0, Vector3.up);
+			transform.rotation= Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(0, Vector3.up), t/2f);
 		}
 		RaycastHit2D obj_below = Physics2D.Raycast (groundCheck.position, -Vector2.up, 1f, whatIsPlatform);
 		RaycastHit2D obj_between = Physics2D.Raycast (headCheck.position, -Vector2.up, 3f, whatIsPlatform);		
 		RaycastHit2D obj_up = Physics2D.Raycast (headCheck.position, Vector2.up, 1f, whatIsPlatform);
 		//bool colBelow = (obj_below.collider.transform.position.y + obj_below.collider.bounds.size.y / 2 < groundCheck.position.y);
-        
+		
 		if(obj_up.transform!=null){    
 			obj_up.collider.isTrigger=true;    
 		}
 		if(obj_between.transform!=null){    
 			obj_between.collider.isTrigger=true;    
-        }
+		}
 		if(obj_below.transform!=null && (obj_below.collider.transform.position.y + obj_below.collider.bounds.size.y / 2 < groundCheck.position.y)){ 
 			obj_below.collider.isTrigger=false; 
-        }
-	
+		}
+		
 	}
 	
 	void Update () {
-
+		
 		if (!burried &&(grounded) && Input.GetKeyDown (KeyCode.Space)) {
-				rigidbody2D.AddForce (transform.up * 500);
+			rigidbody2D.AddForce (transform.up * 500);
 		}
 		if (gcc.materialName != "Plant" && !GetComponent<SeedPlanter>().planting && 
 		    !burried && (grounded) && Input.GetKeyDown (KeyCode.DownArrow)) {
 			Dig();
-        }
+		}
 		if (burried && Input.GetKeyDown(KeyCode.UpArrow)) {
 			GoOut();
 		}
@@ -113,7 +133,7 @@ public class CharacterController : MonoBehaviour {
 		} else {
 			headCheck.GetComponent<CircleCollider2D>().isTrigger = false;
 		}
-
+		
 	}
 	void Flip(){
 		facingRight = !facingRight;
@@ -121,26 +141,26 @@ public class CharacterController : MonoBehaviour {
 		theScale.x *= -1;
 		transform.localScale = theScale;
 	}
-
+	
 	void Die() {
 		Transform respawner = GameObject.FindGameObjectWithTag ("Respawn").transform;
 		transform.position = respawner.position;
 	}
-
+	
 	void Dig() {
 		burried = true;
 		rigidbody2D.isKinematic = true;
 		Mainanim.SetBool ("Dig", true);
 		//CAMBIAR ANIMACION
-
+		
 	}
-
+	
 	void GoOut() {
 		burried = false;
 		rigidbody2D.isKinematic = false;
 		Mainanim.SetBool ("Dig", false);
 	}
-
+	
 	void OnTriggerEnter2D(Collider2D coll) {
 		if (coll.gameObject.tag == "switch") {
 			Debug.Log ("Switch");
@@ -150,8 +170,8 @@ public class CharacterController : MonoBehaviour {
 					(mb as Switch).switchThisMotherfucker();
 				}
 			}
-        }
-    }
-
-
+		}
+	}
+	
+	
 }
