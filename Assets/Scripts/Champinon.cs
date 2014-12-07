80731587f81968b19e98dfd22b6787120040c54c
Javier Letelier <letelier.javier@gmail.com>
2014-12-07 13:20:16 -0300

    Champinon con Animacion

--- Assets/Scripts/Champinon.cs
+++ Assets/Scripts/Champinon.cs
@@ -3,16 +3,26 @@ using System.Collections;
 
 public class Champinon : MonoBehaviour{
 	// Use this for initialization
+	Animator anim;
+	bool wt=false;
 	void Start () {
 		gameObject.AddComponent<IPlant> ();
+		anim = this.GetComponentInParent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (wt) {
+			anim.SetBool ("Jump", false);
+			wt=false;
+				}
+		if (anim.GetBool ("Jump"))
+						wt = true;
 	}
 
 	void OnCollisionEnter2D(Collision2D other){
+
+		anim.SetBool ("Jump", true);
 		float nX = Mathf.Min (1.5f * other.gameObject.rigidbody2D.velocity.x, 20f);
 		float nY = Mathf.Min (1.5f * other.gameObject.rigidbody2D.velocity.y, 20f);
 		other.gameObject.rigidbody2D.velocity=new Vector2 (nX, nY);
