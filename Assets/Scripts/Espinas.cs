2e1d663e638e706a9ad3db826fd4ff61e2de7ee3
Vicente Dragicevic <vrdragicevic@gmail.com>
2014-12-08 22:54:33 -0300

    Nivel 3 casi listo. Problema: cuando toca las espinas se devuelve al principio pero el groundCheck se queda en el mismo lugar (entonces no puede hacer nada el mockle)..

---
+++ Assets/Scripts/Espinas.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Espinas : MonoBehaviour {
+	public Transform spawner;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		other.gameObject.transform.position = spawner.transform.position;
+	}
+}
